8138eb29f63c4d4a31c351e755e8d1d7b790e228
someonesatan <someonesatan@gmail.com>
2024-02-16 19:45:29 +0200

    the first one

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `furniture`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-vanilla-extract"]
+};
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import * as React from "react"
+
+const IndexPage = () => {
+  return (
+    <p>Hello PajGUN!</p>
+  )
+}
+
+export default IndexPage
+
+export const Head = () => <title>Home Page</title>
