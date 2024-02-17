a99c36ded1cf10d1c85b89972d53c2411bdda650
павел фрончик <100878035+PavelIN@users.noreply.github.com>
2024-02-17 20:38:57 +0300

    add images

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,22 @@ module.exports = {
     title: `furniture`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-vanilla-extract"]
+  plugins: ["gatsby-plugin-vanilla-extract",`gatsby-plugin-image`,
+  `gatsby-plugin-sharp`,
+  `gatsby-transformer-sharp`,{
+    resolve: '@chakra-ui/gatsby-plugin',
+    options: {
+      /**
+       * @property {boolean} [resetCSS=true]
+       * if false, this plugin will not use `<CSSReset />
+       */
+      resetCSS: true,
+      /**
+       * @property {number} [portalZIndex=undefined]
+       * The z-index to apply to all portal nodes. This is useful
+       * if your app uses a lot z-index to position elements.
+       */
+      portalZIndex: undefined,
+    },
+  },]
 };
