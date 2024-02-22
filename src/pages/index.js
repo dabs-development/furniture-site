7ac1f5531fdcb1911975d11edc3e2dcb3d88ddf4
павел фрончик <100878035+PavelIN@users.noreply.github.com>
2024-02-22 03:02:41 +0300

    add contacts

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,13 @@ import * as React from "react"
 
 import Header from "../components/Header"
 import Footer from "../components/Footer"
-
+import Contacts from "../components/Contacts"
 const IndexPage = () => {
 
   return (
     <>
     <Header/>
+    <Contacts/>
     <Footer/>
 </>
   )
