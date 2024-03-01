f50e1efcdf0c0c5639db1bf8c0522bf936660373
павел фрончик <100878035+PavelIN@users.noreply.github.com>
2024-03-01 18:29:43 +0300

    add bg size

--- src/components/Stock.jsx
+++ src/components/Stock.jsx
@@ -2,13 +2,18 @@ import * as React from "react";
 import { Box, Wrap, WrapItem, Text, Flex, Link, Image, Grid, border ,Button,} from "@chakra-ui/react";
 import { wrap } from "framer-motion";
 
+import fon3 from "../images/fon3.jpg";
+import fon4 from "../images/fon4.webp";
+import fon5 from "../images/fon5.webp";
+
+
 const Stock = ({ openModal }) => 
 
 <Flex  bg="#f1f3f9" flexDirection="column">
 <Text margin="auto" fontSize="3rem" fontWeight="bold">Наши акции</Text>
     <Flex margin='auto'  flexDirection={{xl:'row',lg:'column',md:'column',sm:"column"}} color="#fff">
-       <Flex width={400} height={200} position="relative" bg="rgb(233, 219, 217)" marginTop={10} marginRight={{xl:'10',lg:'0',md:'0',sm:'0'}} borderEndRadius={5} boxShadow="0px 0px 15px 4px rgba(0,0,0,0.3);">
-        <Flex width='68%' bg="#303d4d" opacity="0.3"  borderEndRadius={5}>
+       <Flex width={400} height={200} position="relative" backgroundImage={fon3} backgroundPosition="center" marginTop={10} marginRight={{xl:'10',lg:'0',md:'0',sm:'0'}} borderEndRadius={5} boxShadow="0px 0px 15px 4px rgba(0,0,0,0.3);">
+        <Flex width='68%' bg="#303d4d" opacity="0.5"  borderEndRadius={5}>
         <br/>
         </Flex>
         <Box position="absolute" border="2px solid #ffd700" top={5} left={2.5} >
@@ -20,8 +25,8 @@ const Stock = ({ openModal }) =>
             </Box>
         <Flex><br/></Flex>
        </Flex>
-       <Flex width={400} height={200} position="relative" bg="rgb(233, 219, 217)" marginTop={10} borderEndRadius={5} boxShadow="0px 0px 15px 4px rgba(0,0,0,0.3);">
-        <Flex width='68%' bg="#303d4d" opacity="0.3"  borderEndRadius={5}>
+       <Flex width={400} height={200} position="relative" backgroundImage={fon4} backgroundPosition="center" marginTop={10} borderEndRadius={5} boxShadow="0px 0px 15px 4px rgba(0,0,0,0.3);">
+        <Flex width='68%' bg="#303d4d" opacity="0.5"  borderEndRadius={5}>
         <br/>
         </Flex>
         <Box position="absolute" border="2px solid #ffd700" top={5} left={2.5}>
@@ -34,8 +39,8 @@ const Stock = ({ openModal }) =>
         <Flex><br/></Flex>
        </Flex>
        </Flex>
-       <Flex width={400} height={200} position="relative" bg="rgb(233, 219, 217)" margin='auto' marginTop={10} borderEndRadius={5} boxShadow="0px 0px 15px 4px rgba(0,0,0,0.3);">
-        <Flex width='68%' bg="#303d4d" opacity="0.3" borderEndRadius={5}>
+       <Flex width={400} height={200} position="relative" backgroundImage={fon5} backgroundPosition="center" margin='auto' marginTop={10} borderEndRadius={5} boxShadow="0px 0px 15px 4px rgba(0,0,0,0.3);">
+        <Flex width='68%' bg="#303d4d" opacity="0.5" borderEndRadius={5}>
         <br/>
         </Flex>
         <Box position="absolute" border="2px solid #ffd700" top={5} left={2.5} color="#fff">
