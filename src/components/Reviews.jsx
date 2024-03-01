6e1e14a9d4aae3bfcd122c802c2c21eca9e68e16
someonesatan <someonesatan@gmail.com>
2024-03-01 18:48:50 +0200

    fix scrollbar

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -10,6 +10,7 @@ import fonn from "../images/fon2.jpeg";
 
 const settings = {
   dots: true,
+  arrows: false,
   infinite: true,
   speed: 400,
   slidesToShow: 1,
@@ -31,14 +32,38 @@ const Reviews = ({ openModal }) => {
           position="relative"
           height="max-content"
         >
-          <Box position="absolute" zIndex={10} top={{ xl: '90px', lg: '90px', md: '90px', sm: '50px' }} left={{ xl: '110px', lg: '70px', md: '50px', sm: '20px' }} display="flex" flexDirection="column">
-            <Text fontSize="2rem" fontWeight="bold">КОРПУСНАЯ МЕБЕЛЬ<br />НА ЗАКАЗ</Text>
-            <Text marginBottom={5} fontSize="1.5rem">Функциональная, красивая и долговечная мебель – это Наша мебель</Text>
-            <Button bg="#79B52D" colorScheme="teal" onClick={openModal} width={350}>
+          <Box
+            position="absolute"
+            zIndex={10}
+            top={{ xl: "90px", lg: "90px", md: "90px", sm: "50px" }}
+            left={{ xl: "110px", lg: "70px", md: "50px", sm: "20px" }}
+            display="flex"
+            flexDirection="column"
+          >
+            <Text fontSize="2rem" fontWeight="bold">
+              КОРПУСНАЯ МЕБЕЛЬ
+              <br />
+              НА ЗАКАЗ
+            </Text>
+            <Text marginBottom={5} fontSize="1.5rem">
+              Функциональная, красивая и долговечная мебель – это Наша мебель
+            </Text>
+            <Button
+              bg="#79B52D"
+              colorScheme="teal"
+              onClick={openModal}
+              width={350}
+            >
               ПОЛУЧИТЬ КОНСУЛЬТАЦИЮ
             </Button>
           </Box>
-          <Flex width="100%" bg="#000" height="32rem" opacity="0.6" flexDirection="column">
+          <Flex
+            width="100%"
+            bg="#000"
+            height="32rem"
+            opacity="0.6"
+            flexDirection="column"
+          >
             <br />
           </Flex>
         </Flex>
@@ -50,14 +75,38 @@ const Reviews = ({ openModal }) => {
           position="relative"
           height="max-content"
         >
-          <Box position="absolute" zIndex={10} top={{ xl: '90px', lg: '90px', md: '90px', sm: '50px' }} left={{ xl: '110px', lg: '70px', md: '50px', sm: '20px' }} display="flex" flexDirection="column">
-            <Text fontSize="2rem" fontWeight="bold">СОЗДАДИМ СОВРЕМЕННУЮ И ПРАКТИЧНУЮ<br />МЕБЕЛЬ ПО ВАШИМ РАЗМЕРАМ</Text>
-            <Text marginBottom={5} paddingTop={5} fontSize="1.5rem">Мы позаботимся о том, чтобы Ваша мебель была собрана специалистом</Text>
-            <Button bg="#79B52D" colorScheme="teal" onClick={openModal} width={350}>
+          <Box
+            position="absolute"
+            zIndex={10}
+            top={{ xl: "90px", lg: "90px", md: "90px", sm: "50px" }}
+            left={{ xl: "110px", lg: "70px", md: "50px", sm: "20px" }}
+            display="flex"
+            flexDirection="column"
+          >
+            <Text fontSize="2rem" fontWeight="bold">
+              СОЗДАДИМ СОВРЕМЕННУЮ И ПРАКТИЧНУЮ
+              <br />
+              МЕБЕЛЬ ПО ВАШИМ РАЗМЕРАМ
+            </Text>
+            <Text marginBottom={5} paddingTop={5} fontSize="1.5rem">
+              Мы позаботимся о том, чтобы Ваша мебель была собрана специалистом
+            </Text>
+            <Button
+              bg="#79B52D"
+              colorScheme="teal"
+              onClick={openModal}
+              width={350}
+            >
               УЗНАТЬ ПОДРОБНЕЕ
             </Button>
           </Box>
-          <Flex width="100%" bg="#000" height="32rem" opacity="0.6" flexDirection="column">
+          <Flex
+            width="100%"
+            bg="#000"
+            height="32rem"
+            opacity="0.6"
+            flexDirection="column"
+          >
             <br />
           </Flex>
         </Flex>
