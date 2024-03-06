f97748f4aa5df0280716f7f3c4ce3c374536f442
павел фрончик <100878035+PavelIN@users.noreply.github.com>
2024-03-06 23:56:04 +0300

    delete useEffect

--- src/pages/hallways.jsx
+++ src/pages/hallways.jsx
@@ -9,15 +9,7 @@ const IndexPage = ({location}) => {
 
     const [dispaly, setDisplay] = React.useState("none");
 
-    React.useEffect(() => {
-      console.log(dispaly)
-      if(location.pathname==="/"){
-        setDisplay("content")
-      }else{
-        setDisplay("none")
-      }
-    }, [dispaly]);
-  
+
 
   return (
     <Layout dispaly={dispaly}>
