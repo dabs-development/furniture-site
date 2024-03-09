978116bb020a169433118f8b1328e78a3df8030b
павел фрончик <100878035+PavelIN@users.noreply.github.com>
2024-03-09 16:43:14 +0300

    add photo shkaff

--- src/model/cabinetPhoto.js
+++ src/model/cabinetPhoto.js
@@ -4,6 +4,9 @@ import shkaf6 from "../images/shkaf(6).jpg";
 import shkaf7 from "../images/shkaf(7).jpg";
 import shkaf8 from "../images/shkaf(8).jpg";
 import shkaf9 from "../images/shkaf(9).jpg";
+import shkaf11 from "../images/shkaf(11).jpg";
+import shkaf12 from "../images/shkaf(12).jpg";
+import shkaf13 from "../images/shkaf(13).jpg";
 
 export default [
     shkaf1,
@@ -12,4 +15,7 @@ export default [
     shkaf7,
     shkaf8,
     shkaf9,
+    shkaf13,
+    shkaf12,
+    shkaf11,
   ];
